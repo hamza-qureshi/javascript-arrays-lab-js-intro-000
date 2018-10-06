f0d0a1cace38ba732738f644bfff497e85f483d9
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-10-06 08:32:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ function destructivelyAppendKitten(name){
   kittens.unshift(name)
 }
 
-function appendKitten (){
-
+function appendKitten (name){
+return [...kittens,name]
 }
+function appendKitten (name){
+return [name,...kittens]
+}
+function appendKitten() {
+  kittens.slice(1)
+}
+function appendKitten (name){
+kittens.slice(0,kittens.length - 1)
+}
+
