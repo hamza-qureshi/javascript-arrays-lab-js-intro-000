69adbe128c258ae0bea9625d1eadac9a9fe4745a
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-10-06 08:21:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
-function destructivelyAppendKitten(arr){
-  kittens.slice(...arr,"Ralph")
+function destructivelyAppendKitten(name){
+  kittens.pop(name)
 }
 
 function appendKitten (){
